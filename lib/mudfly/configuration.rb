89fb94ec18fa1ea6c4ecc9358118c2489a2faa90
chinmay-amuratech <chinmay@amuratech.com>
2016-03-09 15:32:34 +0530

    changed made in configuration.rb

--- lib/mudfly/configuration.rb
+++ lib/mudfly/configuration.rb
@@ -5,25 +5,31 @@ module Mudfly
   module Configuration
 
     VALID_CONFIGURATION_KEYS = [:api_key, :user_agent, :endpoint, :version, :locale]
-    DEFAULT_USER_AGENT       = "Mudfly Ruby Gem #{Mudfly::VERSION}"
-    DEFAULT_ENDPOINT         = 'https://www.googleapis.com/pagespeedonline'
-    DEFAULT_VERSION          = :v2
-    DEFAULT_LOCALE           = :en_US
+    DEFAULT_USER_AGENT = "Mudfly Ruby Gem #{Mudfly::VERSION}"
+    DEFAULT_ENDPOINT   = 'https://www.googleapis.com/pagespeedonline'
+    DEFAULT_VERSION   = :v2
+    DEFAULT_LOCALE     = :en_US
 
     attr_accessor *VALID_CONFIGURATION_KEYS
 
     def self.extended(base)
-      base.api_key     = nil
-      base.user_agent  = DEFAULT_USER_AGENT
-      base.endpoint    = DEFAULT_ENDPOINT
-      base.version     = DEFAULT_VERSION
-      base.locale      = DEFAULT_LOCALE
+
+      base.api_key    = nil
+      base.user_agent = DEFAULT_USER_AGENT
+      base.endpoint   = DEFAULT_ENDPOINT
+      base.version   = DEFAULT_VERSION
+      base.locale     = DEFAULT_LOCALE
+
     end
 
     # Allow block configuration
 
     def configure
+
       yield self
+
     end
+
   end # Configuration
+
 end # Mudfly
